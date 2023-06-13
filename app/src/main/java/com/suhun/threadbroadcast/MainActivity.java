981bc73df611743af20fc774d69ae5213eeba7b0
suhunlin <lin.suhun@msa.hinet.net>
2023-06-13 12:15:24 +0800

    Add layout

--- app/src/main/java/com/suhun/threadbroadcast/MainActivity.java
+++ app/src/main/java/com/suhun/threadbroadcast/MainActivity.java
@@ -3,12 +3,20 @@ package com.suhun.threadbroadcast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView showResult1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showResult1 = findViewById(R.id.outputResult1);
+    }
+
+    public void showThread1Result(View view){
+
     }
 }
